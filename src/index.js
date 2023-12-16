615481645c5dc5e866a67e2f491e53dc93fbb544
Mohit Joshi <b210055@nitsikkim.ac.in>
2023-12-16 19:02:36 +0530

    Project skeleton setup done

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { PORT } = require('./config/serverConfig');
+const app = express();
+
+const prepareAndStratServer = () => {
+    app.listen(PORT, () => {
+        console.log(`Server Started at port: ${PORT}`);
+    })
+}
+
+prepareAndStratServer();
