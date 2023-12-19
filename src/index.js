c859997612ff83fc1c18174337a0bd9c25030ff8
Mohit Joshi <b210055@nitsikkim.ac.in>
2023-12-19 16:27:57 +0530

    Added getById function in user repo

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ const { PORT } = require('./config/serverConfig');
 const app = express();
 const apiRoutes = require('./routes/index');
 
+// const UserRepository = require('./repository/user-repository');
+
+
 const prepareAndStratServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));   
     app.use('/api', apiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server Started at port: ${PORT}`);
+        // const userRepository = new UserRepository();
+        // const result = await userRepository.getById(1);
+        // console.log(result);
     });
 }
 
