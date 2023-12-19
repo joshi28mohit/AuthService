fba2e389e69b76c9f3ac7c03975124ed34500533
Mohit Joshi <b210055@nitsikkim.ac.in>
2023-12-19 17:41:09 +0530

    Added token generation logic from signing in

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -45,7 +45,28 @@ const destroy = async (req,res) => {
     }
 }
 
+const signIn = async (req, res) => {
+    try {
+        const response = await userService.signIn(req.body.email, req.body.password);
+        return res.status(200).json({
+            message: 'Successfully Signed in',
+            data: response,
+            success: true,
+            err: {}
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Something went wrong',
+            data: {},
+            success: false,
+            err: error
+        })
+    }
+}
+
 module.exports = {
     create,
-    destroy
+    destroy,
+    signIn
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const UserController = require('../../controllers/user-controller');
+const UserService = require('../../services/user-service');
 
 const router = express.Router();
 
 router.post('/signup', UserController.create);
+router.post('/signin', UserController.signIn);
 
 module.exports = router;
