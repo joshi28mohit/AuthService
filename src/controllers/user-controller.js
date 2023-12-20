31958ce7cd0bd2390afc6361054064e803ab3eab
Mohit Joshi <b210055@nitsikkim.ac.in>
2023-12-20 17:15:36 +0530

    Added the logic to check whether the user is admin or not

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -86,9 +86,30 @@ const isAuthenticated = async (req, res) => {
 }
 }
 
+const isAdmin = async (req, res) => {
+    try {
+        const response = await userService.isAdmin(req.body.id);
+        return res.status(200).json({
+            message: 'Successfully fetched whether user is admin or not',
+            data: response,
+            success: true,
+            err: {}
+        })
+    } catch (error) {
+    console.log(error);
+        return res.status(500).json({
+            message: 'Something went wrong',
+            data: {},
+            success: false,
+            err: error
+        })
+}
+}
+
 module.exports = {
     create,
     destroy,
     signIn,
-    isAuthenticated
+    isAuthenticated,
+    isAdmin
 }
